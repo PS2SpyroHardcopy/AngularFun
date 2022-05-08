b23f6c9979770c1551a8e73b52c262aaabba9f5f
kkonorev <konorevkirill@gmail.com>
2022-05-08 19:49:37 -0400

    fixed task issue

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componenets/header/header.component';
 import { ButtonComponent } from './componenets/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
+import { TasksComponent } from './componenets/tasks/tasks.component';
 
 @NgModule({
   declarations: [
